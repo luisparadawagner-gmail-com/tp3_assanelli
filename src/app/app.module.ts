bc20d85523ecba2970b76dbcb2a3f16246f3cc1c
nico1512 <nicoassanelli@hotmail.com>
2020-05-13 12:30:17 -0300

    Se agregan diseño en formulario
    Se agregan diseño en 2 botones del formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,27 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioPeliculaComponent } from './formulario/formulario-pelicula/formulario-pelicula.component';
-import { from } from 'rxjs';
+
 import { HijoActorComponent } from './Data-Binding/hijo-actor/hijo-actor.component';
 import { PadrePeliculaComponent } from './Data-Binding/padre-pelicula/padre-pelicula.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioPeliculaComponent,
     HijoActorComponent,
-    PadrePeliculaComponent
+    PadrePeliculaComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    MatButtonModule,
     BrowserAnimationsModule
   ],
   providers: [],
